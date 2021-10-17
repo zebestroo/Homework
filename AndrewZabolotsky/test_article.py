56206c22b18ecff58a24ac8fda827325a064a53b
zebestroo <zebestroo@gmail.com>
2021-10-17 20:29:58 +0300

    Move files into AndrewZabolotsky dir

---
+++ AndrewZabolotsky/test_article.py
@@ -0,0 +1,23 @@
+import article
+import datetime
+import unittest
+import xml.etree.ElementTree as ET
+
+
+class TestArticle(unittest.TestCase):
+    def test_parse_from_xml(self):
+        item = """
+                <item>
+                    <title>Title</title>
+                    <link>https://news.yahoo.com</link>
+                    <pubDate>2021-10-14T21:17:20Z</pubDate>
+                </item>
+                """
+        pub = article.Article.parse_from_xml(ET.fromstring(item))
+        self.assertEqual(pub.title, 'Title')
+        self.assertEqual(pub.link, 'https://news.yahoo.com')
+        self.assertEqual(pub.date, datetime.datetime(2021, 10, 14, 21, 17, 20))
+
+
+if __name__ == '__main__':
+    unittest.main()
